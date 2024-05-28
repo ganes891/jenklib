cb2a2522f7918d95a020b3dce732eaea1f5ec575
ganes891 <ganesan.kandasami@gmail.com>
2024-05-28 16:32:16 +0800

    added library for jenk

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePush.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePush.groovy
@@ -1,4 +1,4 @@
-def call(String project, String ImageTag, String hubUser){
+def call(String ImageTag, String hubUser){
      withCredentials([usernamePassword(
              credentialsId: "hubUser",
              usernameVariable: "USER",
@@ -7,5 +7,5 @@ def call(String project, String ImageTag, String hubUser){
          sh "docker login -u '$USER' -p '$PASS'"
      }
      sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"   
+     sh "docker image push ${hubUser}/${project}:latest"   
  }
