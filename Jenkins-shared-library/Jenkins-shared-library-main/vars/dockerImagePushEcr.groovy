2a152e27d9bb6bb31964e34cfd0efee16b7379e8
ganes891 <ganesan.kandasami@gmail.com>
2024-12-26 14:29:41 +0800

    test

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePushEcr.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePushEcr.groovy
@@ -7,5 +7,5 @@ def call(project){
      )]) {
          sh """aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"""
      }
-     sh """docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"""
+     sh """docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}/${PROJECT}:${IMAGE_TAG}"""
  }
