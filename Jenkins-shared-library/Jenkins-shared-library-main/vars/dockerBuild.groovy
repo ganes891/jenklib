a11dd78b04e1bebb37b15a88a081574d5934232b
ganes891 <ganesan.kandasami@gmail.com>
2024-12-27 15:20:26 +0800

    lib and jenk changes

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerBuild.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerBuild.groovy
@@ -2,7 +2,7 @@
 def call(project){
     sh """
     sudo docker image build -t ${project} .
-    sudo docker image tag ${PROJECT} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}/${PROJECT}:${IMAGE_TAG}
+    sudo docker image tag ${PROJECT} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}
     """
 }
 
--- Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePushEcr.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePushEcr.groovy
@@ -7,5 +7,5 @@ def call(project){
      )]) {
          sh """aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | sudo docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"""
      }
-     sh """sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}/${PROJECT}:${IMAGE_TAG}"""
+     sh """sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"""
  }
