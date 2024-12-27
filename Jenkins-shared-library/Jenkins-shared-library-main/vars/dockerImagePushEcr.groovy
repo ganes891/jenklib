b753fbafe02fc06759baad2157261dc5c3270102
ganes891 <ganesan.kandasami@gmail.com>
2024-12-27 15:17:02 +0800

    lib and jenk changes

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePushEcr.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePushEcr.groovy
@@ -5,7 +5,7 @@ def call(project){
              usernameVariable: "USER",
              passwordVariable: "PASS"
      )]) {
-         sh """aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"""
+         sh """aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | sudo docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"""
      }
-     sh """docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}/${PROJECT}:${IMAGE_TAG}"""
+     sh """sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}/${PROJECT}:${IMAGE_TAG}"""
  }
