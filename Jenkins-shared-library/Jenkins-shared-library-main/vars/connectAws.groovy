f8bf2eb18df4bb015d38b3792968b858c67cc836
ganes891 <ganesan.kandasami@gmail.com>
2025-01-09 15:02:55 +0800

    lib and jenk changes

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/connectAws.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/connectAws.groovy
@@ -1,13 +1,12 @@
 def call(project){
      withCredentials([usernamePassword(
-             credentialsId: "AWS_ACCESS_KEY_ID",
-             usernameVariable: "AWS_ACCESS_KEY_ID",
-             credentialsId: "AWS_SECRET_KEY_ID",
-             passwordVariable: "AWS_SECRET_KEY_ID",
+             credentialsId: "AWS_ACCESS_KEY",
+             usernameVariable: "USER",
+             passwordVariable: "PASS"
      )]) {
-        sh "aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID""
-        sh "aws configure set aws_secret_access_key "$AWS_SECRET_KEY_ID""
-        sh "aws configure set region "${params.Region}""
+        sh "aws configure set aws_access_key_id "$USER""
+        sh "aws configure set aws_secret_access_key "$PASS""
+        sh "aws configure set region "${AWS_REGION}""
         //sh aws eks --region ${AWS_REGION} update-kubeconfig --name ${CLUSTER_NAME}
      }
      sh "aws eks --region ${AWS_DEFAULT_REGION} update-kubeconfig --name ${CLUSTER_NAME}"
