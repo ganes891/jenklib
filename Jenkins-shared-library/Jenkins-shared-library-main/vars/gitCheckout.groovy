10f219ecf34837004bfd1c19b259db875264d07b
ganes891 <ganesan.kandasami@gmail.com>
2025-01-27 13:02:41 +0800

    lib and jenk changes

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/gitCheckout.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/gitCheckout.groovy
@@ -1,8 +1,5 @@
 def call(project) {
-  def branch = env.BRANCH
-  def url = env.GIT_URL
-  def creds = env.GITHUB_CREDENTIALS
-
+    
     checkout([
         $class: 'GitSCM',
         branches: [[name: "${BRANCH}" ]],
