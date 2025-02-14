888b988b6c9c2d792e55fda6d8a4f9c9238b5b2b
ganes891 <ganesan.kandasami@gmail.com>
2025-02-14 14:42:15 +0800

    lib and jenk changes

--- Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePush.groovy
+++ Jenkins-shared-library/Jenkins-shared-library-main/vars/dockerImagePush.groovy
@@ -4,7 +4,7 @@ def call(project){
              usernameVariable: "USER",
              passwordVariable: "PASS"
      )]) {
-         sh """podman login -u '$USER' -p '$PASS' quay.io"""
+         sh """sudo podman login -u '$USER' -p '$PASS' quay.io"""
      }
     // sh "sudo podman image push ${hubUser}/${project}:${imageTag}"  
      sh """sudo podman push quay.io/ganesan_kandasamy/${PROJECT}:${IMAGE_TAG}"""
